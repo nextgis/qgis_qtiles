2ba69507577e15285db57ae0cc2a8b97d46af2eb
Alexander Bruy <alexander.bruy@gmail.com>
2015-01-13 19:37:38 +0200

    fix

--- qtilesdialog.py
+++ qtilesdialog.py
@@ -24,54 +24,36 @@
 # MA 02110-1335 USA.
 #
 #******************************************************************************
-
-
 import locale
 import math
 import operator
-
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
-
 from qgis.core import *
-
 import tilingthread
-
 from ui.ui_qtilesdialogbase import Ui_Dialog
-
 import qtiles_utils as utils
 
-
 class QTilesDialog(QDialog, Ui_Dialog):
-
     def __init__(self, iface):
         QDialog.__init__(self)
         self.setupUi(self)
-
         self.iface = iface
         self.workThread = None
-
         self.FORMATS = {
             self.tr('ZIP archives (*.zip *.ZIP)') : '.zip',
             self.tr('MBTiles databases (*.mbtiles *.MBTILES)') : '.mbtiles'}
-
         self.settings = QSettings('NextGIS', 'QTiles')
         self.grpParameters.setSettings(self.settings)
-
         self.btnOk = self.buttonBox.button(QDialogButtonBox.Ok)
         self.btnClose = self.buttonBox.button(QDialogButtonBox.Close)
-
         self.rbOutputZip.toggled.connect(self.__toggleZipTarget)
         self.rbExtentLayer.toggled.connect(self.__toggleLayerSelector)
         self.chkLockRatio.stateChanged.connect(self.__toggleHeightEdit)
         self.spnTileWidth.valueChanged.connect(self.__updateTileSize)
-
         self.btnBrowse.clicked.connect(self.__selectOutput)
-
         self.cmbFormat.activated.connect(self.formatChanged)
-
         self.manageGui()
-
     def formatChanged(self):
         if self.cmbFormat.currentText()=='JPG':
             self.spnTransparency.setEnabled(False)
@@ -79,7 +61,6 @@ class QTilesDialog(QDialog, Ui_Dialog):
         else:
             self.spnTransparency.setEnabled(True)
             self.spnQuality.setEnabled(False)
-
     def manageGui(self):
         layers = utils.getMapLayers()
         relations = self.iface.legendInterface().groupLayerRelationship()
@@ -89,91 +70,67 @@ class QTilesDialog(QDialog, Ui_Dialog):
                 self.cmbLayers.addItem(layer[1], layer[0])
             else:
                 self.cmbLayers.addItem('%s - %s' % (layer[1], groupName), layer[0])
-
         self.rbOutputZip.setChecked(self.settings.value('outputToZip', True, type=bool))
         self.rbOutputDir.setChecked(self.settings.value('outputToDir', False, type=bool))
-
         if self.rbOutputZip.isChecked():
             self.leDirectoryName.setEnabled(False)
         else:
             self.leZipFileName.setEnabled(False)
-
         self.cmbLayers.setEnabled(False)
-
         self.leRootDir.setText(self.settings.value('rootDir', 'Mapnik'))
-
         self.rbExtentCanvas.setChecked(self.settings.value('extentCanvas', True, type=bool))
         self.rbExtentFull.setChecked(self.settings.value('extentFull', False, type=bool))
         self.rbExtentLayer.setChecked(self.settings.value('extentLayer', False, type=bool))
-
         self.spnZoomMin.setValue(self.settings.value('minZoom', 0, type=int))
         self.spnZoomMax.setValue(self.settings.value('maxZoom', 18, type=int))
-
         self.chkLockRatio.setChecked(self.settings.value('keepRatio', True, type=bool))
         self.spnTileWidth.setValue(self.settings.value('tileWidth', 256, type=int))
         self.spnTileHeight.setValue(self.settings.value('tileHeight', 256, type=int))
-
         self.spnTransparency.setValue(self.settings.value('transparency', 255, type=int))
         self.spnQuality.setValue(self.settings.value('quality', 70, type=int))
         self.cmbFormat.setCurrentIndex(int(self.settings.value('format', 0)))
         self.chkAntialiasing.setChecked(self.settings.value('enable_antialiasing', False, type=bool))
         self.chkTMSConvention.setChecked(self.settings.value('use_tms_filenames', False, type=bool))
-
         self.chkWriteMapurl.setChecked(self.settings.value("write_mapurl", False, type=bool))
         self.chkWriteViewer.setChecked(self.settings.value("write_viewer", False, type=bool))
-
         self.formatChanged()
-
     def reject(self):
         QDialog.reject(self)
-
     def accept(self):
         if self.rbOutputZip.isChecked():
             output = self.leZipFileName.text()
         else:
             output = self.leDirectoryName.text()
-
         if not output:
             QMessageBox.warning(self, self.tr('No output'), self.tr('Output path is not set. Please enter correct path and try again.'))
             return
-
         fileInfo = QFileInfo(output)
         if fileInfo.isDir() and not len(QDir(output).entryList(QDir.Dirs | QDir.Files | QDir.NoDotAndDotDot)) == 0:
             res = QMessageBox.warning(self, self.tr('Directory not empty'), self.tr('Selected directory is not empty. Continue?'), QMessageBox.Yes | QMessageBox.No)
             if res == QMessageBox.No:
                 return
-
         if self.spnZoomMin.value() > self.spnZoomMax.value():
             QMessageBox.warning(self, self.tr('Wrong zoom'), self.tr('Maximum zoom value is lower than minimum. Please correct this and try again.'))
             return
-
         self.settings.setValue('rootDir', self.leRootDir.text())
-
         self.settings.setValue('outputToZip', self.rbOutputZip.isChecked())
         self.settings.setValue('outputToDir', self.rbOutputDir.isChecked())
-
         self.settings.setValue('extentCanvas', self.rbExtentCanvas.isChecked())
         self.settings.setValue('extentFull', self.rbExtentFull.isChecked())
         self.settings.setValue('extentLayer', self.rbExtentLayer.isChecked())
-
         self.settings.setValue('minZoom', self.spnZoomMin.value())
         self.settings.setValue('maxZoom', self.spnZoomMax.value())
-
         self.settings.setValue('keepRatio', self.chkLockRatio.isChecked())
         self.settings.setValue('tileWidth', self.spnTileWidth.value())
         self.settings.setValue('tileHeight', self.spnTileHeight.value())
-
         self.settings.setValue('format', self.cmbFormat.currentIndex ())
         self.settings.setValue('transparency', self.spnTransparency.value())
         self.settings.setValue('quality', self.spnQuality.value())
         self.settings.setValue('enable_antialiasing', self.chkAntialiasing.isChecked())
         self.settings.setValue('use_tms_filenames', self.chkTMSConvention.isChecked())
-
         self.settings.setValue('write_mapurl', self.chkWriteMapurl.isChecked())
         self.settings.setValue('write_viewer', self.chkWriteViewer.isChecked())
-
         canvas = self.iface.mapCanvas()
-
         if self.rbExtentCanvas.isChecked():
             extent = canvas.extent()
         elif self.rbExtentFull.isChecked():
@@ -181,82 +138,51 @@ class QTilesDialog(QDialog, Ui_Dialog):
         else:
             layer = utils.getLayerById(self.cmbLayers.itemData(self.cmbLayers.currentIndex()))
             extent = canvas.mapRenderer().layerExtentToOutputExtent(layer, layer.extent())
-
         extent = QgsCoordinateTransform(canvas.mapRenderer().destinationCrs(), QgsCoordinateReferenceSystem('EPSG:4326')).transform(extent)
-
         arctanSinhPi = math.degrees(math.atan(math.sinh(math.pi)))
         extent = extent.intersect(QgsRectangle(-180, -arctanSinhPi, 180, arctanSinhPi))
-
         layers = canvas.mapSettings().layers()
         writeMapurl = self.chkWriteMapurl.isEnabled() and self.chkWriteMapurl.isChecked()
         writeViewer = self.chkWriteViewer.isEnabled() and self.chkWriteViewer.isChecked()
-
-        self.workThread = tilingthread.TilingThread(
-                layers,
-                extent,
-                self.spnZoomMin.value(),
-                self.spnZoomMax.value(),
-                self.spnTileWidth.value(),
-                self.spnTileHeight.value(),
-                self.spnTransparency.value(),
-                self.spnQuality.value(),
-                self.cmbFormat.currentText(),
-                fileInfo,
-                self.leRootDir.text(),
-                self.chkAntialiasing.isChecked(),
-                self.chkTMSConvention.isChecked(),
-                writeMapurl,
-                writeViewer)
+        self.workThread = tilingthread.TilingThread(layers,extent,self.spnZoomMin.value(),self.spnZoomMax.value(),self.spnTileWidth.value(),self.spnTileHeight.value(), self.spnTransparency.value(),self.spnQuality.value(),self.cmbFormat.currentText(),fileInfo,self.leRootDir.text(),self.chkAntialiasing.isChecked(),self.chkTMSConvention.isChecked(),writeMapurl,writeViewer)
         self.workThread.rangeChanged.connect(self.setProgressRange)
         self.workThread.updateProgress.connect(self.updateProgress)
         self.workThread.processFinished.connect(self.processFinished)
         self.workThread.processInterrupted.connect(self.processInterrupted)
-
         self.btnOk.setEnabled(False)
         self.btnClose.setText(self.tr('Cancel'))
         self.buttonBox.rejected.disconnect(self.reject)
         self.btnClose.clicked.connect(self.stopProcessing)
-
         self.workThread.start()
-
     def setProgressRange(self, message, value):
         self.progressBar.setFormat(message)
         self.progressBar.setRange(0, value)
-
     def updateProgress(self):
         self.progressBar.setValue(self.progressBar.value() + 1)
-
     def processInterrupted(self):
         self.restoreGui()
-
     def processFinished(self):
         self.stopProcessing()
         self.restoreGui()
-
     def stopProcessing(self):
         if self.workThread is not None:
             self.workThread.stop()
             self.workThread = None
-
     def restoreGui(self):
         self.progressBar.setFormat('%p%')
         self.progressBar.setRange(0, 1)
         self.progressBar.setValue(0)
-
         self.buttonBox.rejected.connect(self.reject)
         self.btnClose.clicked.disconnect(self.stopProcessing)
         self.btnClose.setText(self.tr('Close'))
         self.btnOk.setEnabled(True)
-
     def __toggleZipTarget(self, checked):
         self.leZipFileName.setEnabled(checked)
         self.leDirectoryName.setEnabled(not checked)
         self.chkWriteMapurl.setEnabled(not checked)
         self.chkWriteViewer.setEnabled(not checked)
-
     def __toggleLayerSelector(self, checked):
         self.cmbLayers.setEnabled(checked)
-
     def __toggleHeightEdit(self, state):
         if state == Qt.Checked:
             self.lblHeight.setEnabled(False)
@@ -265,32 +191,23 @@ class QTilesDialog(QDialog, Ui_Dialog):
         else:
             self.lblHeight.setEnabled(True)
             self.spnTileHeight.setEnabled(True)
-
     @pyqtSlot(int)
     def __updateTileSize(self, value):
         if self.chkLockRatio.isChecked():
             self.spnTileHeight.setValue(value)
-
     def __selectOutput(self):
         lastDirectory = self.settings.value('lastUsedDir', '.')
-
         if self.rbOutputZip.isChecked():
             outPath, outFilter = QFileDialog.getSaveFileNameAndFilter(self, self.tr('Save to file'), lastDirectory, ';;'.join(self.FORMATS.iterkeys()), self.FORMATS.keys()[self.FORMATS.values().index('.zip')])
-
             if not outPath:
                 return
-
             if not outPath.lower().endswith(self.FORMATS[outFilter]):
                 outPath += self.FORMATS[outFilter]
-
             print outPath
-
             self.leZipFileName.setText(outPath)
         else:
             outPath = QFileDialog.getExistingDirectory(self, self.tr('Save to directory'), lastDirectory, QFileDialog.ShowDirsOnly)
             if not outPath:
                 return
-
             self.leDirectoryName.setText(outPath)
-
         self.settings.setValue('lastUsedDir', QFileInfo(outPath).absoluteDir().absolutePath())
--- tilingthread.py
+++ tilingthread.py
@@ -24,38 +24,26 @@
 # MA 02110-1335 USA.
 #
 #******************************************************************************
-
-
 import math
 import time
 from string import Template
-
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
-
 from qgis.core import *
-
 from tile import Tile
 from writers import *
-
 import resources_rc
 
-
 class TilingThread(QThread):
-
     rangeChanged = pyqtSignal(str, int)
     updateProgress = pyqtSignal()
     processFinished = pyqtSignal()
     processInterrupted = pyqtSignal()
-
-    def __init__(self, layers, extent, minZoom, maxZoom, width, height, transp,
-                 quality, format, outputPath, rootDir, antialiasing, tmsConvention,
-                 mapUrl, viewer):
+    def __init__(self, layers, extent, minZoom, maxZoom, width, height, transp, quality, format, outputPath, rootDir, antialiasing, tmsConvention, mapUrl, viewer):
         QThread.__init__(self, QThread.currentThread())
         self.mutex = QMutex()
         self.stopMe = 0
         self.interrupted = False
-
         self.layers = layers
         self.extent = extent
         self.minZoom = minZoom
@@ -66,15 +54,12 @@ class TilingThread(QThread):
             self.rootDir = rootDir
         else:
             self.rootDir = 'tileset_%s' % unicode(time.time()).split('.')[0]
-
         self.antialias = antialiasing
         self.tmsConvention = tmsConvention
-
         self.format = format
         self.quality = quality
         self.mapurl = mapUrl
         self.viewer = viewer
-
         if self.output.isDir():
             self.mode = 'DIR'
         elif self.output.suffix().lower() == "zip":
@@ -82,27 +67,17 @@ class TilingThread(QThread):
         elif self.output.suffix().lower() == 'mbtiles':
             self.mode = 'MBTILES'
             self.tmsConvention = True
-
         self.interrupted = False
         self.tiles = []
-
         myRed = QgsProject.instance().readNumEntry('Gui', '/CanvasColorRedPart', 255)[0]
         myGreen = QgsProject.instance().readNumEntry('Gui', '/CanvasColorGreenPart', 255)[0]
         myBlue = QgsProject.instance().readNumEntry('Gui', '/CanvasColorBluePart', 255)[0]
-
-        if int(QT_VERSION_STR[2]) >= 8:
-            self.color = QColor(myRed, myGreen, myBlue, transp)
-        else:
-            self.color = qRgba(myRed, myGreen, myBlue, transp)
-
+        self.color = QColor(myRed, myGreen, myBlue, transp)
         image = QImage(width, height, QImage.Format_ARGB32_Premultiplied)
-
         self.projector = QgsCoordinateTransform(QgsCoordinateReferenceSystem('EPSG:4326'), QgsCoordinateReferenceSystem('EPSG:3395'))
-
         self.scaleCalc = QgsScaleCalculator()
         self.scaleCalc.setDpi(image.logicalDpiX())
         self.scaleCalc.setMapUnits(QgsCoordinateReferenceSystem('EPSG:3395').mapUnits())
-
         self.settings = QgsMapSettings()
         self.settings.setLayers(self.layers)
         self.settings.setBackgroundColor(self.color)
@@ -115,66 +90,49 @@ class TilingThread(QThread):
         self.settings.setFlag(QgsMapSettings.DrawLabeling, True)
         if self.antialias:
             self.settings.setFlag(QgsMapSettings.Antialiasing, True)
-
     def run(self):
         self.mutex.lock()
         self.stopMe = 0
         self.mutex.unlock()
-
         if self.mode == 'DIR':
             self.writer = DirectoryWriter(self.output, self.rootDir)
             if self.mapurl:
                 self.writeMapurlFile()
-
             if self.viewer:
                 self.writeLeafletViewer()
         elif self.mode == 'ZIP':
             self.writer = ZipWriter(self.output, self.rootDir)
         elif self.mode == 'MBTILES':
             self.writer = MBTilesWriter(self.output, self.rootDir)
-
         self.rangeChanged.emit(self.tr('Searching tiles...'), 0)
-
         useTMS = 1
         if self.tmsConvention:
             useTMS = -1
-
         self.countTiles(Tile(0, 0, 0, useTMS))
-
         if self.interrupted:
             del self.tiles[:]
             self.tiles = None
-
             self.processInterrupted.emit()
-
         self.rangeChanged.emit(self.tr('Rendering: %v from %m (%p%)'), len(self.tiles))
-
         for t in self.tiles:
             self.render(t)
-
             self.updateProgress.emit()
-
             self.mutex.lock()
             s = self.stopMe
             self.mutex.unlock()
             if s == 1:
                 self.interrupted = True
                 break
-
         self.writer.finalize()
-
         if not self.interrupted:
             self.processFinished.emit()
         else:
             self.processInterrupted.emit()
-
     def stop(self):
         self.mutex.lock()
         self.stopMe = 1
         self.mutex.unlock()
-
         QThread.wait(self)
-
     def writeMapurlFile(self):
         filePath = '%s/%s.mapurl' % (self.output.absoluteFilePath(), self.rootDir)
         tileServer = 'tms' if self.tmsConvention else 'google'
@@ -184,35 +142,22 @@ class TilingThread(QThread):
             mapurl.write('%s=%s\n' % ('maxzoom', self.maxZoom))
             mapurl.write('%s=%f %f\n' % ('center', self.extent.center().x(), self.extent.center().y()))
             mapurl.write('%s=%s\n' % ('type', tileServer))
-
     def writeLeafletViewer(self):
         templateFile = QFile(':/resources/viewer.html')
         if templateFile.open(QIODevice.ReadOnly | QIODevice.Text):
             viewer = MyTemplate(unicode(templateFile.readAll()))
             tilesDir = '%s/%s' % (self.output.absoluteFilePath(), self.rootDir)
             useTMS = 'true' if self.tmsConvention else 'false'
-            substitutions = {'tilesdir'    : tilesDir,
-                             'tilesetname' : self.rootDir,
-                             'tms'         : useTMS,
-                             'centerx'     : self.extent.center().x(),
-                             'centery'     : self.extent.center().y(),
-                             'avgzoom'     : (self.maxZoom + self.minZoom) / 2,
-                             'maxzoom'     : self.maxZoom
-                            }
-
+            substitutions = {'tilesdir': tilesDir, 'tilesetname': self.rootDir, 'tms': useTMS, 'centerx': self.extent.center().x(),'centery': self.extent.center().y(),'avgzoom': (self.maxZoom + self.minZoom) / 2,'maxzoom': self.maxZoom}
             filePath = '%s/%s.html' % (self.output.absoluteFilePath(), self.rootDir)
             with open(filePath, 'w') as fOut:
                 fOut.write(viewer.substitute(substitutions))
-
             templateFile.close()
-
     def countTiles(self, tile):
         if self.interrupted or not self.extent.intersects(tile.toRectangle()):
             return
-
         if self.minZoom <= tile.z and tile.z <= self.maxZoom:
             self.tiles.append(tile)
-
         if tile.z < self.maxZoom:
             for x in xrange(2 * tile.x, 2 * tile.x + 2, 1):
                 for y in xrange(2 * tile.y, 2 * tile.y + 2, 1):
@@ -222,10 +167,8 @@ class TilingThread(QThread):
                     if s == 1:
                         self.interrupted = True
                         return
-
                     subTile = Tile(x, y, tile.z + 1, tile.tms)
                     self.countTiles(subTile)
-
     def render(self, tile):
         scale = self.scaleCalc.calculate(self.projector.transform(tile.toRectangle()), self.width)
         self.settings.setExtent(self.projector.transform(tile.toRectangle()))
@@ -237,6 +180,5 @@ class TilingThread(QThread):
 
 class MyTemplate(Template):
     delimiter = '@'
-
     def __init__(self, templateString):
         Template.__init__(self, templateString)
